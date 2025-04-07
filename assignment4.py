fd11e7b51e963f44526353dd13a8b671654350bb
jaldakkour <jaaldakkour1@buffs.wtamu.edu>
2025-04-06 21:10:12 -0500

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,10 @@
+#Run server and check API
+#Run following commands to create the database, and then run server: 
+
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py runserver
+
+
+#Now go to your browser and type the following URL
+
