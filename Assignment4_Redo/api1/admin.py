94a8197d02abf4c7d376ad7b7159c821e7d7f852
jaldakkour <jaaldakkour1@buffs.wtamu.edu>
2025-05-09 16:06:42 -0500

    Update admin.py

--- Assignment4_Redo/api1/admin.py
+++ Assignment4_Redo/api1/admin.py
@@ -2,7 +2,6 @@
 from django.contrib import admin
 from .models import SanareSoma
 
-# Register your models here.
 @admin.register(SanareSoma)
 class SanareSomaAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
