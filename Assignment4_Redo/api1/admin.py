1d884c21f070c4c6fdadf63be88015f049eea4fa
jaldakkour <jaaldakkour1@buffs.wtamu.edu>
2025-05-08 19:10:52 -0500

    Update admin.py

--- Assignment4_Redo/api1/admin.py
+++ Assignment4_Redo/api1/admin.py
@@ -1,1 +1,27 @@
 
+from django.contrib import admin
+from .models import SanareSoma
+
+# Register your models here.
+@admin.register(SanareSoma)
+class SanareSomaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ('title',)
+    list_filter = ('title',)
+    ordering = ('title',)
+    list_per_page = 10
+    list_editable = ('description',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description')
+        }),
+    )
+    prepopulated_fields = {'description': ('title',)}   
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
