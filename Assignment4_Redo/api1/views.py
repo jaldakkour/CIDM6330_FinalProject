06da032cfec486dd5bf02e106fe5e5d17d85dc35
jaldakkour <jaaldakkour1@buffs.wtamu.edu>
2025-05-08 19:18:51 -0500

    Update views.py

--- Assignment4_Redo/api1/views.py
+++ Assignment4_Redo/api1/views.py
@@ -1,1 +1,8 @@
+from rest_framework import viewsets
+ 
+from .serializers import SanareSomaSerializer
+from .models import SonareSoma
 
+class GeeksViewSet(viewsets.ModelViewSet):
+    queryset = SonareSoma.objects.all()
+    serializer_class = SanareSomaSerializer
