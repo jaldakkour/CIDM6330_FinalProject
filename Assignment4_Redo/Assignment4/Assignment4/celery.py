69fc281fa10c55cf4194ad3195ce57bae8165c82
jaldakkour <jaaldakkour1@buffs.wtamu.edu>
2025-05-10 18:54:58 -0500

    Update celery.py

--- Assignment4_Redo/Assignment4/Assignment4/celery.py
+++ Assignment4_Redo/Assignment4/Assignment4/celery.py
@@ -1,1 +1,17 @@
+import os
+from celery import Celery
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+
+app = Celery('SonareSoma')
+
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
