b7a1b08a705a681a7b16e75c8085ede6da0995b7
jaldakkour <jaaldakkour1@buffs.wtamu.edu>
2025-05-10 17:24:01 -0500

    Delete Assignment4_Redo directory

--- Assignment4_Redo/api1/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
- 
-from .serializers import SanareSomaSerializer
-from .models import SonareSoma
-
-class GeeksViewSet(viewsets.ModelViewSet):
-    queryset = SonareSoma.objects.all()
-    serializer_class = SanareSomaSerializer
