8049b354c340093caacd302a8d3668eab8f6d20e
jaldakkour <jaaldakkour1@buffs.wtamu.edu>
2025-05-10 18:33:32 -0500

    Delete forms.py

--- forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.http import Http404
-from .models import Goal, UserProfile, Professional
-from .
