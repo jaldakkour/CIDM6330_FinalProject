4d71d5dd4b35118d4406e34f7f6d7c4cc50ca02a
jaldakkour <jaaldakkour1@buffs.wtamu.edu>
2025-05-11 21:08:41 -0500

    Delete CIDM6330 directory

--- CIDM6330/__init__.py
+++
@@ -1,4 +0,0 @@
-# filepath: /Users/janealdakkour/Desktop/School/6330 Soft Arch/CIDM6330-main 3/CIDM6330/CIDM6330/__init__.py
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
--- CIDM6330/asgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-
-# Set the default settings module for the 'asgi' application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CIDM6330.settings')
-
-# Get the ASGI application
-application = get_asgi_application()
